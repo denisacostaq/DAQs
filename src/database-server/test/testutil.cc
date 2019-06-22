f6c985e22c2cf81b87c8e11d71258d5ef09bdb0a
Alvaro Denis <denisacostaq@gmail.com>
2019-06-22 15:56:26 -0400

    add test utils ref #3

---
+++ src/database-server/test/testutil.cc
@@ -0,0 +1,49 @@
+/*! @brief This file have the implementation for test utils.
+    @file testutil.cc
+    @author Alvaro Denis <denisacostaq@gmail.com>
+    @date 6/22/2019
+
+    @copyright
+    @attention <h1><center><strong>COPYRIGHT &copy; 2019 </strong>
+    [<strong>denisacostaq</strong>][denisacostaq-URL].
+    All rights reserved.</center></h1>
+    @attention This file is part of [<strong>DAQs</strong>][DAQs-URL].
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+    - 1. Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    - 2. Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    - 3. Neither the name of the University nor the names of its contributors
+      may be used to endorse or promote products derived from this software
+      without specific prior written permission.
+
+    THIS PRODUCT IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+    ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+    THIS PRODUCT, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+    
+    [denisacostaq-URL]: https://about.me/denisacostaq "Alvaro Denis Acosta"
+    [DAQs-URL]: https://github.com/denisacostaq/DAQs "DAQs"
+ */
+
+#include "src/database-server/test/testutil.h"
+
+#include <boost/filesystem.hpp>
+
+std::string get_random_sqlite_file_path() noexcept {
+    const auto &fileRel = boost::filesystem::unique_path("%%%%_%%%%_%%%%_%%%%.db");
+    const auto &dirAbs = boost::filesystem::temp_directory_path() / "DAQs";
+    boost::filesystem::create_directories(dirAbs);
+    const auto &fileAbs = dirAbs / fileRel;
+    return fileAbs.string();
+}
